53c9398af222557f7b711846e9d04a75478ca24b
DenisDubinin <dadubinin@gmail.com>
2016-11-06 11:17:19 +0100

    tower attack

--- tower.cpp
+++ tower.cpp
@@ -1,6 +1,7 @@
 #include "tower.h"
 #include "bullet.h"
 #include "game.h"
+#include "enemy.h"
 
 #include <QPixmap>
 #include <QVector>
@@ -55,15 +56,21 @@ Tower::Tower(QGraphicsItem *parent) : QObject(), QGraphicsPixmapItem(parent)
 
     // connect a timer to attackTarget
     QTimer * timer = new QTimer;
-    connect(timer, SIGNAL(timeout()),this,SLOT(attackTarget()));
+    connect(timer, SIGNAL(timeout()),this,SLOT(aquireTarget()));
 
     timer->start(1000);
 
     // set attack_dest;
-    attack_dest = QPointF(100, 0);
+    attack_dest = QPointF(800, 0);
 }
 
-void Tower::attackTarget()
+double Tower::distanceTo(QGraphicsItem *item)
+{
+    QLineF ln(pos(), item->pos());
+    return ln.length();
+}
+
+void Tower::fire()
 {
     Bullet * bullet = new Bullet;
 
@@ -73,4 +80,33 @@ void Tower::attackTarget()
     int angle = -1 * ln.angle();
     bullet->setRotation(angle);
     game->scene->addItem(bullet);
+}
+
+void Tower::aquireTarget()
+{
+    // get list of all colliding with attack_area
+    QList<QGraphicsItem *> colliding_items = attack_area->collidingItems();
+
+    if (colliding_items.size() == 1) {
+        has_target = false;
+        return;
+    }
+
+    double closest_dist = 300;
+    QPointF closest_pt(0, 0);
+
+    for (size_t i = 0, n = colliding_items.size(); i < n; i++) {
+        Enemy * enemy = dynamic_cast<Enemy *>(colliding_items[i]);
+        if (enemy) {
+            double this_dist = distanceTo(enemy);
+            if (this_dist < closest_dist) {
+                closest_dist = this_dist;
+                closest_pt = colliding_items[i]->pos();
+                has_target = true;
+            }
+        }
+    }
+
+    attack_dest = closest_pt;
+    fire();
 }
--- tower.h
+++ tower.h
@@ -13,15 +13,17 @@ class Tower: public QObject, public QGraphicsPixmapItem
     Q_OBJECT
 public:
     Tower(QGraphicsItem *parent = 0);
-
+    double distanceTo(QGraphicsItem * item);
+    void fire();
 public slots:
-    void attackTarget();
+    void aquireTarget();
 private:
     QGraphicsPolygonItem *attack_area;
     QPointF attack_dest;
     QPixmap qpixmap;
     int half_tower_height;
     int half_tower_width;
+    bool has_target;
 };
 
 #endif // TOWER_H
